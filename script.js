6c1fd31ea4fdfc5ee0b1cd26bf0893630f7a0095
JOANIR FERNANDO RIBEIRO <108032158+JoanirRibeiro@users.noreply.github.com>
2025-07-29 12:25:39 -0300

    .

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function inserirNome(){
     let nomeUsuario = prompt("Qual o seu nome?");
     let elemento = document.querySelector("#nome-usuario");
+    elemento.textContent = nomeUsuario;
 }
 
 inserirNome();
 
-let linguagens = ["JavaScript","Python","C++"];
+const linguagens = ["JavaScript","Python","C++"];
 
 console.log(linguagens[0]) // “JavaScript”
 console.log(linguagens[1]) // “Python”
